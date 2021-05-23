70d7c8b6b02b5714b6b8ec59a1dacdd1b55ed684
Slava <SlavaShensev@gmail.com>
2021-05-23 23:50:55 +0300

    add client

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,15 @@
+// Core
 import React from 'react'
+import {ApolloProvider} from '@apollo/react-hooks'
+
+// Other
+import {client} from "./init/client";
 
 export const App = () => {
-
     return (
-        <>
+        <ApolloProvider client={client}>
             <h1> App </h1>
-        </>
+        </ApolloProvider>
     )
 
 }
