dc69befdff3496c9261f9bd968c1de200825371a
Slava <SlavaShensev@gmail.com>
2021-05-24 00:28:06 +0300

    add counter

---
+++ src/app/bus/pet/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Counter} from "./counter";
+
+export const Pet = () => {
+    return (
+        <>
+            <h2> Pet </h2>
+            < Counter/>
+        </>
+    )
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,18 @@
 // Core
-import React from 'react'
-import {ApolloProvider} from '@apollo/react-hooks'
+import React from 'react';
+import {ApolloProvider} from '@apollo/react-hooks';
 
 // Other
 import {client} from "./init/client";
+import {Pet} from "./bus/pet";
 
 export const App = () => {
     return (
         <ApolloProvider client={client}>
             <h1> App </h1>
+            <Pet/>
+
         </ApolloProvider>
     )
 
-}
+};
